946fd5aa489bf6911243c088e29e8eaa3f15823b
Wix Remote Machine <support@wix.com>
2025-11-16 17:21:23 +0000

    Set logo as site favicon

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -3,6 +3,9 @@ export const Head = () => {
     <>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      {/* Favicon */}
+      <link rel="icon" type="image/png" href="https://static.wixstatic.com/media/e6a693_cd0a358e5da4470e9b2d9902a0701688~mv2.png" />
+      <link rel="shortcut icon" type="image/png" href="https://static.wixstatic.com/media/e6a693_cd0a358e5da4470e9b2d9902a0701688~mv2.png" />
       {/* Fonts */}
       <link rel="preconnect" href="https://static.parastorage.com" />
     </>
